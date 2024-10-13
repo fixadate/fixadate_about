6b62234e461108b88dc9d48e3f7ab37e018d5259
Oscar(재웅) Lee <69746360+wooooooongs@users.noreply.github.com>
2024-10-13 20:19:47 +0900

    🔧 vite, tsconfig 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     base: '/fixadate_about/',
+    preview: {
+        host: '0.0.0.0',
+        port: 8080,
+    },
 });
