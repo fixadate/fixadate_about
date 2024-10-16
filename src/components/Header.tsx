99f1598ff1f9671d4bb3664c69ac2780377f4a3d
Oscar(재웅) Lee <69746360+wooooooongs@users.noreply.github.com>
2024-10-16 17:30:47 +0900

    💄 Adjust Header UI: padding, size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,11 +6,11 @@ export const Header = ({ className }: { className?: string }) => {
             className={`${className} duration-250 fixed left-6 right-6 top-6 z-50 overflow-hidden rounded-full shadow-lg transition-opacity`}
             style={{ boxShadow: '0px 0px 18px 0px rgba(156, 164, 190, 0.30)' }}
         >
-            <nav className='flex w-full items-center justify-between bg-fixadate-white px-8 py-2'>
+            <nav className='flex w-full items-center justify-between bg-fixadate-white py-1 pl-5 pr-6'>
                 {/* Left */}
                 <Link to='/fixadate_about/'>
-                    <div className='flex items-center gap-3'>
-                        <span className='h-12 w-12'>
+                    <div className='flex items-center gap-0.5'>
+                        <span className='h-11 w-11'>
                             <img
                                 src='images/app_icon.png'
                                 alt='icon'
@@ -18,12 +18,12 @@ export const Header = ({ className }: { className?: string }) => {
                             />
                         </span>
 
-                        <span className='text-xl font-bold'>Fixadate</span>
+                        <span className='fixadate text-xl font-bold'>Fixadate</span>
                     </div>
                 </Link>
 
                 {/* Right */}
-                <div className='flex gap-6 text-sm font-medium text-fixadate-gray-5 sm:text-base'>
+                <div className='flex gap-6 text-sm font-medium text-fixadate-gray-6 sm:text-base'>
                     <Link to='/fixadate_about/'>ABOUT</Link>
                     <Link to='/fixadate_about/tech-stack'>TECH</Link>
                 </div>
